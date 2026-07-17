23170073ce3f8918c792bf330a0b67f1bd879cd9
Ricardo Montoya <ricardo.johny.montoya@gmail.com>
2026-07-16 23:03:15 -0600

    primer commit

---
+++ BibliotecaMVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using BibliotecaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Libros()
+        {
+            return View();
+        }
+        
+        public IActionResult Autores()
+        {
+            return View();
+        }
+
+        public IActionResult Categorías()
+        {
+            return View();
+        }
+
+        public IActionResult Préstamos()
+        {
+            return View();
+        }
+
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+
+        public IActionResult Acercade()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
